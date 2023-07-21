c244355a5ed4e0e1c7f90b59f057b2cc4c34fb19
iSalah-YT <kurditstation@gmail.com>
2023-07-21 23:19:21 +0100

    project done

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "About Salah Media",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Salah Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque excepturi
+        veniam nostrum quidem enim dolore soluta molestias vel quos numquam
+        ullam facere delectus amet similique voluptas nemo fugit tempore dicta,
+        eius, exercitationem eos! Suscipit itaque deserunt doloremque, eos saepe
+        exercitationem sequi, consequatur nisi est aperiam, at similique fugit
+        voluptate vero!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim esse
+        illum exercitationem deleniti, aperiam nisi, perferendis suscipit,
+        quaerat voluptates laboriosam eos error. Dolor nulla repellendus, magnam
+        quasi consequuntur porro iure.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
